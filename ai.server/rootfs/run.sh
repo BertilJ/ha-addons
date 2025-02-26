67fcccbcce6f1dc2d675f0ec895172e8bd7087ea
Abertil <ante.jonasson@gmail.com>
2025-02-26 13:17:12 +0100

    modified:   ai.server/rootfs/run.sh

--- ai.server/rootfs/run.sh
+++ ai.server/rootfs/run.sh
@@ -8,33 +8,33 @@ CONFIG_PATH=/data/options.json
 # Ensure Home Assistant storage exists
 mkdir -p /share/ai-server/data
 mkdir -p /share/ai-server/modules
-mkdir -p /share/ai-server/downloads/modules/packages
-mkdir -p /share/ai-server/opt
+# mkdir -p /share/ai-server/downloads/modules/packages
+# mkdir -p /share/ai-server/opt
 
 
-# Bind /etc/codeproject/ai to /share/ai-server/data (Already working)
-if [ ! -L "/etc/codeproject/ai" ]; then
-    rm -rf /etc/codeproject/ai
-    ln -s /share/ai-server/data /etc/codeproject/ai
-fi
+# # Bind /etc/codeproject/ai to /share/ai-server/data (Already working)
+# if [ ! -L "/etc/codeproject/ai" ]; then
+#     rm -rf /etc/codeproject/ai
+#     ln -s /share/ai-server/data /etc/codeproject/ai
+# fi
 
-# Bind /app/modules to /share/ai-server/modules (Primary Module Location)
-if [ ! -L "/app/modules" ]; then
-    rm -rf /app/modules
-    ln -s /share/ai-server/modules /app/modules
-fi
+# # Bind /app/modules to /share/ai-server/modules (Primary Module Location)
+# if [ ! -L "/app/modules" ]; then
+#     rm -rf /app/modules
+#     ln -s /share/ai-server/modules /app/modules
+# fi
 
-# Bind /app/modules to /share/ai-server/modules (Primary Module Location according to documentation)
-if [ ! -L "/opt/codeproject/ai" ]; then
-    rm -rf /opt/codeproject/ai
-    ln -s /share/ai-server/opt /opt/codeproject/ai
-fi
+# # Bind /app/modules to /share/ai-server/modules (Primary Module Location according to documentation)
+# if [ ! -L "/opt/codeproject/ai" ]; then
+#     rm -rf /opt/codeproject/ai
+#     ln -s /share/ai-server/opt /opt/codeproject/ai
+# fi
 
-# Bind /app/downloads/modules/packages to /share/ai-server/downloads/modules/packages (Temporary Module Downloads)
-if [ ! -L "/app/downloads/modules/packages" ]; then
-    rm -rf /app/downloads/modules/packages
-    ln -s /share/ai-server/downloads/modules/packages /app/downloads/modules/packages
-fi
+# # Bind /app/downloads/modules/packages to /share/ai-server/downloads/modules/packages (Temporary Module Downloads)
+# if [ ! -L "/app/downloads/modules/packages" ]; then
+#     rm -rf /app/downloads/modules/packages
+#     ln -s /share/ai-server/downloads/modules/packages /app/downloads/modules/packages
+# fi
 
 
 # Debug output to check if mapping works
