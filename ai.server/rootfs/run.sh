43d72b72fe6449f8fc37c2bef42c3fc01361c6af
Abertil <ante.jonasson@gmail.com>
2025-02-26 09:49:16 +0100

    modified:   ai.server/Dockerfile 	modified:   ai.server/config.yaml 	modified:   ai.server/rootfs/run.sh

--- ai.server/rootfs/run.sh
+++ ai.server/rootfs/run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 CONFIG_PATH=/data/options.json
-export MODULES_PATH=$(jq --raw-output '.MODULES_PATH // empty' $CONFIG_PATH)
-export DATA_DIR=$(jq --raw-output '.DATA_DIR // empty' $CONFIG_PATH)
 
+# Load paths from options.json
+export MODULES_PATH=$(jq --raw-output '.MODULES_PATH // "/app/modules"' $CONFIG_PATH)
+export DATA_DIR=$(jq --raw-output '.DATA_DIR // "/etc/codeproject/ai"' $CONFIG_PATH)
+
+# Ensure directories exist
 mkdir -p "$MODULES_PATH"
 mkdir -p "$DATA_DIR"
 
-cat /app/server/appsettings.json  | sed -e 's/\/\/ .*$//g' | jq ".ModuleOptions.ModulesPath=\"$MODULES_PATH\"" > /app/server/appsettings.json.new
+# Fix appsettings.json to use the correct modules path
+cat /app/server/appsettings.json | sed -e 's/\/\/ .*$//g' | jq ".ModuleOptions.ModulesPath=\"$MODULES_PATH\"" > /app/server/appsettings.json.new
 mv /app/server/appsettings.json.new /app/server/appsettings.json
 
+# Run the AI server with the correct application data directory
 cd /app/server && dotnet ./CodeProject.AI.Server.dll --ApplicationDataDir="$DATA_DIR"
